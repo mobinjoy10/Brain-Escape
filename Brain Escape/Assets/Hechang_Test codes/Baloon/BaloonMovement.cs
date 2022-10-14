65df3a9e3dd073585905e664329e3e75f78f9993
Hechang Khan <71941895+The-Hechang@users.noreply.github.com>
2022-10-14 23:53:25 +0600

    Balloon improved

--- Brain Escape/Assets/Hechang_Test codes/Baloon/BaloonMovement.cs
+++ Brain Escape/Assets/Hechang_Test codes/Baloon/BaloonMovement.cs
@@ -8,7 +8,9 @@ public class BaloonMovement : MonoBehaviour
 
     [SerializeField] Transform balloonMovePoint1;
     [SerializeField] Transform balloonMovePoint2;
+    [SerializeField] Transform balloonMovePoint3;
 
+    int target = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,30 @@ public class BaloonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == 0)
+        {
+            transform.position = (Vector3.MoveTowards(transform.position, balloonMovePoint1.position, 0.5f * Time.deltaTime));
+            if (Vector3.Distance(transform.position, balloonMovePoint1.position) < 0.5)
+            {
+                target = 1;
+            }
+        }
+        else if (target == 1)
+        {
+            transform.position = (Vector3.MoveTowards(transform.position, balloonMovePoint2.position, 0.5f * Time.deltaTime));
+            if (Vector3.Distance(transform.position, balloonMovePoint2.position) < 0.5)
+            {
+                target = 2;
+            }
+        }
+        else if (target == 2)
+        {
+            transform.position = (Vector3.MoveTowards(transform.position, balloonMovePoint3.position, 0.5f * Time.deltaTime));
+            if (Vector3.Distance(transform.position, balloonMovePoint3.position) < 0.5)
+            {
+                target = 0;
+            }
+        }
+    
     }
 }
