39ce5357a37bfc9d67e804e5bc7a094aa5532b3c
Hechang Khan <71941895+The-Hechang@users.noreply.github.com>
2022-10-14 22:12:04 +0600

    Baloon Burst System

---
+++ Brain Escape/Assets/Hechang_Test codes/Baloon/BaloonPop.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaloonPop : MonoBehaviour
+{
+
+    Animator baloonAnim;
+
+    [SerializeField] string triggerName;
+
+    [SerializeField] Transform KeyObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baloonAnim = GetComponent<Animator>();
+    }
+
+    //This code will be called by Bullet Object to reduce update calls.
+    //This code manages the burst animation of the Baloon.
+    void baloonBurstAnim()
+    {
+        baloonAnim.SetTrigger(triggerName);
+    }
+
+    //This code will be called from the animator
+    //This code destroys/disables the baloon and spawns a key.
+    void baloonDestroy()
+    {
+
+        //Spawn the key giving an illusion of the key coming out of the baloon after it pops
+        if(KeyObject)
+            Instantiate(KeyObject, transform.position, Quaternion.identity);
+
+        gameObject.SetActive(false);
+    }
+}
