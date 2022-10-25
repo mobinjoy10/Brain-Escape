bb727072008028524993307b42d60c0c9a126d7d
Hechang Khan <71941895+The-Hechang@users.noreply.github.com>
2022-10-25 21:18:57 +0600

    Update Levelstate.cs

--- Brain Escape/Assets/Hechang_Test codes/Levelstate.cs
+++ Brain Escape/Assets/Hechang_Test codes/Levelstate.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Levelstate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Levelstate Instance;
+
+    public LevelStates state;
+
+    public static event Action<LevelStates> OnLevelStateChanged;
+
+    private void Awake()
     {
-        
+        Instance = this;
+    }
+   
+    public enum LevelStates{
+        Begin,
+        BaloonPop,
+        LightDestroy,
+        DoorUnlocked,
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateLevelState(LevelStates newState)
     {
-        
+        state = newState;
+
+        switch (newState)
+        {
+            case LevelStates.Begin:
+                break;
+            case LevelStates.BaloonPop:
+                break;
+            case LevelStates.LightDestroy:
+                break;
+            case LevelStates.DoorUnlocked:
+                break;
+
+        }
+
+        OnLevelStateChanged?.Invoke(newState);
+
     }
 }
