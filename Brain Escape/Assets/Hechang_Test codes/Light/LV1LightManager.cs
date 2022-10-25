735c0b3bb5d007ef3b80fe871db02dc519f0b0df
Hechang Khan <71941895+The-Hechang@users.noreply.github.com>
2022-10-25 23:53:17 +0600

    Light improved

---
+++ Brain Escape/Assets/Hechang_Test codes/Light/LV1LightManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LV1LightManager : MonoBehaviour
+{
+    [SerializeField] GameObject HomeLight;
+    [SerializeField] GameObject MoonLight;
+    [SerializeField] GameObject MoonLightReflection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HomeLight.SetActive(true);
+        MoonLight.SetActive(false);
+        MoonLightReflection.SetActive(false);
+    }
+
+    void HomelightHit()
+    {
+        MoonLightReflection.SetActive(true);
+        MoonLight.SetActive(true);
+
+        HomelightBlast();
+    }
+
+    void HomelightBlast()
+    {
+        HomeLight.SetActive(false);
+    }
+    
+}
