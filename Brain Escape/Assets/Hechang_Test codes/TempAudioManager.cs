a6db05fec11884963fff38c4e61d4a460c3769e7
Hechang Khan <71941895+The-Hechang@users.noreply.github.com>
2022-11-13 16:51:43 +0600

    some little things

---
+++ Brain Escape/Assets/Hechang_Test codes/TempAudioManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds
+{
+    public AudioSource source;
+    public bool is3D;
+    public float waitingTime;
+}
+
+public class TempAudioManager : MonoBehaviour
+{
+    public Sounds sounds;
+
+}
